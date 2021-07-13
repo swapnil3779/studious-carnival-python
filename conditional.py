908a0bfd0ae726c5ccce39d063ce70af89906339
swapnil3779 <87384077+swapnil3779@users.noreply.github.com>
2021-07-13 21:51:35 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,11 @@
+money= 70
+item= 60
+chocolate=20
+if(money-item>chocolate):
+    print('yes you can by chocolate') 
+elif(money-item>chocolate):
+    
+    print('you are low on money')
+
+else:
+    print('no you cant buy chocolate')
